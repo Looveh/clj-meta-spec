d0e6ffbb864b0164413cafaeda8a3066ae76e2f6
Niclas Nilsson <niclas.nilsson@protonmail.com>
2022-10-02 18:16:02 +0200

    Initial commit

---
+++ src/clj_spec_meta/main.clj
@@ -0,0 +1,30 @@
+(ns clj-spec-meta.main
+  (:require [clojure.spec.alpha :as s]))
+
+(defn- myfn
+  {::spec {:args (s/cat :a int? :b boolean?)
+           :ret string?}
+   :doc "my function"}
+  [a b]
+  (str a " " b))
+
+(defmacro fdef-from-meta [{:keys [namespaces functions meta-kw]}]
+  (let [meta-kw# (or meta-kw ::spec)
+        fn-vars# (->> (map ns-interns namespaces)
+                      (mapcat vals)
+                      (filter #(and (fn? (var-get %))
+                                    (get (meta %) meta-kw#)))
+                      (concat functions))]
+    `(do
+       ~@(map (fn [fn-var]
+                `(s/fdef ~(symbol fn-var)
+                   :args ~(get-in (meta fn-var) [meta-kw# :args])
+                   :ret ~(get-in (meta fn-var) [meta-kw# :ret])
+                   :fn ~(get-in (meta fn-var) [meta-kw# :fn])))
+              fn-vars#))))
+
+(comment
+  (macroexpand (quote (fdef-from-meta {:namespaces [clj-spec-meta.main]})))
+  (fdef-from-meta {:namespaces [clj-spec-meta.main]})
+  (s/get-spec 'clj-spec-meta.main/myfn)
+  )
