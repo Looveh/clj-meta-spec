e4995a0e8a29e561f2d3f2fafd17222c854bab2a
Niclas Nilsson <niclas.nilsson@protonmail.com>
2022-10-04 20:57:49 +0200

    Add reset-specs?

--- src/clj_spec_meta/main.clj
+++ src/clj_spec_meta/main.clj
@@ -8,26 +8,43 @@
   [a b]
   (str a " " b))
 
-(defmacro fdef-from-meta [{:keys [namespaces functions meta-kw exclude-fns ns-regex]}]
-  (let [meta-kw# (or meta-kw ::spec)
-        ns-matching-re (when ns-regex
+(defn- find-fn-vars [{:keys [exclude-fn ns-regex] fn' :fn ns' :ns}]
+  (let [ns-matching-re (when ns-regex
                          (filter #(re-matches ns-regex (str (ns-name %)))
                                  (all-ns)))
-        fn-vars# (->> (concat namespaces ns-matching-re)
-                      (distinct)
+        fn-in-ns (->> (concat ns' ns-matching-re)
                       (map ns-interns)
                       (mapcat vals)
-                      (filter #(and (fn? (var-get %))
-                                    (get (meta %) meta-kw#)
-                                    (not (contains? exclude-fns %))))
-                      (concat functions))]
-    `(do
-       ~@(map (fn [fn-var]
-                `(s/fdef ~(symbol fn-var)
-                   :args ~(get-in (meta fn-var) [meta-kw# :args])
-                   :ret ~(get-in (meta fn-var) [meta-kw# :ret])
-                   :fn ~(get-in (meta fn-var) [meta-kw# :fn])))
-              fn-vars#))))
+                      (filter #(fn? (var-get %))))]
+    (->> fn-in-ns
+         (filter #(not (contains? exclude-fn %)))
+         (concat fn')
+         (distinct))))
+
+(defmacro fdef-from-meta
+  [{:keys [meta-kw ns-regex exclude-fn reset-specs?]
+    fn' :fn ns' :ns
+    :or {meta-kw ::spec}}]
+  (let [fn-vars (find-fn-vars {:meta-kw meta-kw
+                               :ns ns'
+                               :ns-regex ns-regex
+                               :fn fn'
+                               :exclude-fn exclude-fn})
+        spec-resets (map (fn [fn-var]
+                           (when reset-specs?
+                             `(s/def ~(symbol fn-var) nil)))
+                         fn-vars)
+        spec-defs (map (fn [fn-var]
+                         (let [{args' :args ret' :ret fn' :fn} (get (meta fn-var) meta-kw)]
+                           (when (or args' ret' fn')
+                             `(s/fdef ~(symbol fn-var)
+                                :args ~args'
+                                :ret ~ret'
+                                :fn ~fn'))))
+                       fn-vars)
+        statements (->> (concat spec-resets spec-defs)
+                        (filter some?))]
+    (cons 'do statements)))
 
 (defn fns-without-specs [{:keys [namespaces]}]
   (->> (map ns-interns namespaces)
@@ -42,4 +59,5 @@
   (fdef-from-meta {:ns-regex #".*clj-spec-meta.*"})
   (s/get-spec 'clj-spec-meta.main/myfn)
   (fns-without-specs {:namespaces ['clj-spec-meta.main]})
+  (s/def clj-spec-meta.main/myfn nil)
   )
